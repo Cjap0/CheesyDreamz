c8342e8332592d3e629666e53ab80ae23bb027df
Cjap0 <connorphoenixcoco01@gmail.com>
2024-05-03 10:35:35 +0100

    Added login code and partially implemented order provider to make checkout

--- MyCheeseShop/Model/Order.cs
+++ MyCheeseShop/Model/Order.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public User User { get; set; }
         public List<OrderItem> Items { get; set; }
-        public decimal PriceTotal { get; set; }
-        public decimal Total { get; set; }
+        public DateTime Created { get; set; }
+        public OrderStatus Status { get; set; }
     }
     
 }
--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -35,5 +35,53 @@
             // return the number of items in the cart
             return _items.Count;
         }
+
+        public decimal Total()
+        {
+            //sum of all the items(prices) in the cart
+            return _items.Sum(item => item.Cheese.Price * item.Quantity);
+        }
+        public void RemoveItem(Cheese cheese)
+        {
+            // removes cheese from the cart
+            _items.RemoveAll(item => item.Cheese.Id == cheese.Id);
+            // updates the cart
+            OnCartUpdated?.Invoke();
+
+        }
+
+        public void RemoveItem(Cheese cheese, int quantity)
+        {
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            if (item is not null)
+            {
+                // if the quantity reaches 0 it removes the item from the cart
+                item.Quantity -= quantity;
+                if (item.Quantity <= 0)
+                    _items.Remove(item);
+            }
+            // updates the cart
+            OnCartUpdated?.Invoke();
+        }
+
+        public void clear()
+        {
+            //clears items
+            _items.Clear();
+            OnCartUpdated?.Invoke();    
+        }
+
+        public int GetQuantity(Cheese cheese)
+        {
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id); ;
+            return item?.Quantity ?? 0;
+        }
+
+        public void SetItems(IEnumerable<CartItem> items)
+        {
+            //set items in the cart
+            _items = items.ToList();
+            OnCartUpdated?.Invoke();
+        }
     }
 }
